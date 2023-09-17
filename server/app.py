a0a44456bfef2675e031ac3aaecce6b5d35e53de
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-09-17 12:52:07 -0700

    adds lab

--- server/app.py
+++ server/app.py
@@ -52,6 +52,37 @@ api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
 
+class Login(Resource):
+    
+    def post(self):
+        user = User.query.filter(User.username==request.get_json()['username']).first()
+        session['user_id']= user.id
+
+        response = make_response(jsonify(user.to_dict()), 200)
+
+        return response
+api.add_resource(Login, '/login')
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return make_response(jsonify({'message': '204: No Content'}), 204)
+    
+api.add_resource(Logout, '/logout')
+
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            session['user_id']= user.id
+            response = make_response(jsonify(user.to_dict()), 200)
+            return response
+        
+        else:
+            session['user_id'] =None
+            return make_response(jsonify({}), 401)
+        
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
